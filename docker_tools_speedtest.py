18fe7895d1baa25193837163b7266db966ed35d2
Kritik Modi <61862301+kritikmodi@users.noreply.github.com>
2024-06-20 00:59:59 +0530

    chore: update to single quotes

--- docker_tools_speedtest.py
+++ docker_tools_speedtest.py
@@ -3,11 +3,11 @@ import time
 
 def tag_docker_image_using_crane(image_path, new_tag):
     try:
-        command = ["crane", "tag", image_path, new_tag]
+        command = ['crane', 'tag', image_path, new_tag]
         subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
     except subprocess.CalledProcessError as e:
-        print(f"Error tagging image {image_path} with {new_tag}: {e}")
+        print(f'Error tagging image {image_path} with {new_tag}: {e}')
 
 def tag_docker_image_using_imagetools(image_path, new_tag):
     try:
@@ -18,7 +18,7 @@ def tag_docker_image_using_imagetools(image_path, new_tag):
         subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
     except subprocess.CalledProcessError as e:
-        print(f"Error tagging image {image_path} with {new_tag}: {e}")
+        print(f'Error tagging image {image_path} with {new_tag}: {e}')
 
 def tag_docker_image_using_docker(image_path, image_path_with_new_tag):
     try:
@@ -27,44 +27,44 @@ def tag_docker_image_using_docker(image_path, image_path_with_new_tag):
         subprocess.run(['docker', 'push', image_path_with_new_tag], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         
     except subprocess.CalledProcessError as e:
-        print(f"Error tagging image {image_path} with {new_tag}: {e}")
+        print(f'Error tagging image {image_path} with {new_tag}: {e}')
 
 counter = 1
 
-image_path = input("Enter image path without any tag : ")
+image_path = input('Enter image path without any tag : ')
 
-old_tag = input("Input any old tag to grab the manifest from : ")
+old_tag = input('Input any old tag to grab the manifest from : ')
 
-new_tag = f"testing{counter}"
+new_tag = f'testing{counter}'
 
-image_path_with_old_tag = f"{image_path}:{old_tag}"
+image_path_with_old_tag = f'{image_path}:{old_tag}'
 
-image_path_with_new_tag = f"{image_path}:{new_tag}"
+image_path_with_new_tag = f'{image_path}:{new_tag}'
 
 start = time.time()
 tag_docker_image_using_crane(image_path_with_old_tag, new_tag)
 end = time.time()
 
-print("Crane execution time :", (end-start) * 10**3, "ms")
+print('Crane execution time :', (end-start) * 10**3, 'ms')
 
 counter=counter+1
 
-new_tag = f"testing{counter}"
-image_path_with_new_tag = f"{image_path}:{new_tag}"
+new_tag = f'testing{counter}'
+image_path_with_new_tag = f'{image_path}:{new_tag}'
 
 start = time.time()
 tag_docker_image_using_imagetools(image_path_with_old_tag, image_path_with_new_tag)
 end = time.time()
 
-print("Imagetools execution time :", (end-start) * 10**3, "ms")
+print('Imagetools execution time :', (end-start) * 10**3, 'ms')
 
 counter=counter+1
 
-new_tag = f"testing{counter}"
-image_path_with_new_tag = f"{image_path}:{new_tag}"
+new_tag = f'testing{counter}'
+image_path_with_new_tag = f'{image_path}:{new_tag}'
 
 start = time.time()
 tag_docker_image_using_docker(image_path_with_old_tag, image_path_with_new_tag)
 end = time.time()
 
-print("Docker execution time :", (end-start) * 10**3, "ms")
+print('Docker execution time :', (end-start) * 10**3, 'ms')
